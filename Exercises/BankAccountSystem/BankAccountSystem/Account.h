6195c73edae57823cf4c231b5129a04e7548dec2
John Tungul <jtungul@jtungul-macbookair.roam.corp.google.com>
2015-10-29 21:43:59 -0700

    Adding Account.h file

---
+++ Exercises/BankAccountSystem/BankAccountSystem/Account.h
@@ -0,0 +1,16 @@
+//
+//  Account.h
+//  BankAccountSystem
+//
+//  Created by John Tungul on 10/29/15.
+//  Copyright © 2015 John Tungul. All rights reserved.
+//
+
+#ifndef Account_h
+#define Account_h
+
+class Account{
+    
+};
+
+#endif /* Account_h */
