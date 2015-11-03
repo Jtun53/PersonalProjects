1f58c3591f1ec067cbddddce207d3f3084d03423
jtun53 <jtungul53@yahoo.com>
2015-11-02 20:05:50 -0800

    Checking.cpp Added

---
+++ Exercises/BankAccountSystem/BankAccountSystem/Checking.cpp
@@ -0,0 +1,18 @@
+//
+//  Checking.cpp
+//  BankAccountSystem
+//
+//  Created by John Tungul on 11/2/15.
+//  Copyright © 2015 John Tungul. All rights reserved.
+//
+
+#include <stdio.h>
+#include "Checking.h"
+
+void deposit(double amount){
+    
+};
+
+double withdraw(double amount){
+    return 0.0;
+};
