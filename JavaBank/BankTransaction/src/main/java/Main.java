be1777097a3725458e5f204e05a290672ab64751
Jtun53 <jtungul53@yahoo.com>
2016-01-02 21:59:53 -0800

    Intializing Bank info

--- JavaBank/BankTransaction/src/main/java/Main.java
+++ JavaBank/BankTransaction/src/main/java/Main.java
@@ -1,8 +1,18 @@
+import net.sf.ofx4j.client.*;
+import net.sf.ofx4j.client.impl.FinancialInstitutionServiceImpl;
+import net.sf.ofx4j.client.impl.OFXHomeFIDataStore;
+import net.sf.ofx4j.domain.data.banking.BankAccountDetails;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("test");
-
+		OFXHomeFIDataStore fid = new OFXHomeFIDataStore();
+		fid.setUrl("https://ofx.bankofamerica.com/cgi-forte/ofx?servicename=ofx_2-3&pagename=bofa");
+		FinancialInstitutionData bofa = fid.getInstitutionData("6805");
+		FinancialInstitutionService service = new FinancialInstitutionServiceImpl();
+		FinancialInstitution fi = service.getFinancialInstitution(bofa);
+		
 	}
 
 }
