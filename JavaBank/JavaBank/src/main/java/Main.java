093cc712f5c68b12887d0065a8745dfc79672cb0
Jtun53 <jtungul53@yahoo.com>
2016-01-11 19:48:57 -0800

    Adding JFreeChart for pie chart capability

--- JavaBank/JavaBank/src/main/java/Main.java
+++ JavaBank/JavaBank/src/main/java/Main.java
@@ -14,8 +14,12 @@ Milestone Goal 4: Automatically retrieve transaction info from bank
 */
 public class Main {
 	
-	static double total=0;
-	static final double MONTHLYINCOME = 3000.00;
+	static double total = 0;
+	static double foodTotal = 0;
+	static double groceryTotal = 0;
+	static double billsTotal = 0;
+	static double luxuryTotal = 0;
+	static final double MONTHLYINCOME = 5000.00;
 
 	public static void main(String[] args) throws IOException{
 		OpenCSV reader = new OpenCSV("December.csv");
@@ -24,7 +28,24 @@ public class Main {
 		for (Transaction s : transactions){
 			System.out.println(s);
 			total += s.getCost();
+			if (s.getType() == 1){
+				foodTotal += s.getCost();
+			}
+			else if(s.getType() == 2){
+				groceryTotal += s.getCost();
+			}
+			else if(s.getType() == 3){
+				billsTotal += s.getCost();
+			}
+			else if(s.getType() == 4){
+				luxuryTotal += s.getCost();
+			}
 		}
+	System.out.printf("\nTotal = $%6.2f\n", total);
+	System.out.printf("Food = $%6.2f\n", foodTotal);
+	System.out.printf("Grocery = $%6.2f\n ", groceryTotal);
+	System.out.printf("Bills = $%6.2f\n", billsTotal);
+	System.out.printf("Luxury = $%6.2f ", luxuryTotal);
 	}
 
 }
