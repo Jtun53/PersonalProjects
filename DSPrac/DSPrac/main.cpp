89eff1773644344f8ac3e3a7dd963cf16bd54b6e
jtun53 <jtungul53@yahoo.com>
2016-02-26 10:59:40 -0800

    Removed irrelevant folders and projects. Created DSA practice project

---
+++ DSPrac/DSPrac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DSPrac
+//
+//  Created by John Tungul on 2/26/16.
+//  Copyright © 2016 John Tungul. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
