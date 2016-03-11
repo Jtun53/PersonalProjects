151d18b1272e45b7f6d088c11d831fb8b02cd67b
jtun53 <jtungul53@yahoo.com>
2016-03-11 10:06:10 -0800

    Added opencsv dependency to gradle project

---
+++ JavaBank2/src/main/java/Transaction.java
@@ -0,0 +1,40 @@
+
+public class Transaction {
+    int type;
+    double cost;
+    String description;
+
+    public Transaction (int type, double cost, String description){
+        this.type = type;
+        this.cost = cost;
+        this.description = description;
+    }
+
+    public void setType(int type){
+        this.type = type;
+    };
+
+    public int getType(){
+        return this.type;
+    };
+
+    public void setCost(double cost){
+        this.cost = cost;
+    };
+
+    public double getCost(){
+        return this.cost;
+    };
+
+    public void setDescription(String desc){
+        this.description = desc;
+    };
+
+    public String getDescription(){
+        return this.description;
+    };
+
+    public String toString(){
+        return new String(type + " " + cost + " " + description);
+    };
+}
