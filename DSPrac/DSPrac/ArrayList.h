e31bb1c13ef122d15eafeb70eb7a7161fb2c551e
jtun53 <jtungul53@yahoo.com>
2016-04-26 11:18:50 -0700

    Added ArrayList header file

---
+++ DSPrac/DSPrac/ArrayList.h
@@ -0,0 +1,29 @@
+//
+//  ArrayList.h
+//  DSPrac
+//
+//  Created by John Tungul on 4/26/16.
+//  Copyright © 2016 John Tungul. All rights reserved.
+//
+
+#ifndef ArrayList_h
+#define ArrayList_h
+
+template <class t>
+class ArrayList(){
+private:
+    static const int MAXSIZE = 100;
+    t list[MAXSIZE];
+    int itemCount;
+    int
+    
+public:
+    ArrayList();
+    bool isEmpty();
+    int getLength();
+    bool insert(int pos,const t &data);
+    bool remove(int pos);
+    void clear();
+}
+
+#endif /* ArrayList_h */
